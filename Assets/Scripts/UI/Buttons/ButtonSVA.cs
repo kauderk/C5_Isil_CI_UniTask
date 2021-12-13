0753eb1323ebf636a8a3c7e7da4154e3a1a77f39
KauDerK <estudio1kau@gmail.com>
2021-12-13 04:27:23 -0500

    following TaroDevUITut - btnsSVA - fonts - scroller bg

---
+++ Assets/Scripts/UI/Buttons/ButtonSVA.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(AudioSource))]
+public class ButtonSVA : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] Image _img;
+    [SerializeField] Sprite _defaultSprite, _pressedSprite;
+    [SerializeField] AudioClip _pressAuido, _releaseAudio;
+    [SerializeField] AudioSource _audioSource;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _img.sprite = _pressedSprite;
+        _audioSource.PlayOneShot(_pressAuido);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _img.sprite = _defaultSprite;
+        _audioSource.PlayOneShot(_releaseAudio);
+    }
+}
