f4a2e8ccf74dde473d1d9e9275eac6defeb69e18
KauDerK <estudio1kau@gmail.com>
2021-12-13 01:16:20 -0500

    mc - attractor - attractable - fake 2d

---
+++ Assets/Scripts/Mechanics/Planet Attraction 3D/GravityBody.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+
+    GravityAttractor planet;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        planet = FindObjectOfType<GravityAttractor>();// gameObject.GetComponent<GravityAttractor>();
+        rb = GetComponent<Rigidbody>();
+
+        // Disable rigidbody gravity and rotation as this is simulated in GravityAttractor script
+        rb.useGravity = false;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    void FixedUpdate()
+    {
+        // Allow this body to be influenced by planet's gravity
+        planet.Attract(rb);
+    }
+}
